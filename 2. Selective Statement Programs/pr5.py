ed69d39e2a1d292b03001a19a99fe3b0bcf44a51
Pawan Kumar Sah <120468294+pawan1-tech@users.noreply.github.com>
2023-07-08 23:08:11 +0530

    Update pr5.py

--- 2. Selective Statement Programs/pr5.py
+++ 2. Selective Statement Programs/pr5.py
@@ -1,6 +1,7 @@
 ''' Three numbers form a Pythogorean triple if the sum of the squares of two numbers is equal to the square of the third.
 For example, 3, 4, and 5 form a Pythagorean triple since 3*3 + 4*4 = 5*5.
-Enter the three numbers n1, n2, and n3 (they should be in increasing order). If they form a Pythoagorean triple, then print ‘1," otherwise print ‘0’.'''
+Enter the three numbers n1, n2, and n3 (they should be in increasing order). 
+If they form a Pythoagorean triple, then print ‘1," otherwise print ‘0’.'''
 
 a =int (input('enter the first number :'))
 b =int (input('enter the second number :'))
